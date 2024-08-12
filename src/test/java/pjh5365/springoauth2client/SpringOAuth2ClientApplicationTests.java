ce7f7f44b1187713d7be7e87a8da509d3349913b
박지혁 <pjh5365@naver.com>
2024-08-12 16:00:05 +0900

    Init: 프로젝트 초기세팅

---
+++ src/test/java/pjh5365/springoauth2client/SpringOAuth2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pjh5365.springoauth2client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOAuth2ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
